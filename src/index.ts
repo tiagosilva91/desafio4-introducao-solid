030913a011bd86f6585a04f9dd665cf3a95caba8
silvaTiago91 <tiagofef@gmail.com>
2022-06-02 00:47:36 -0300

    Add swagger

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import express from "express";
+import swaggerUi from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerFiles from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
+
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerFiles));
 
 app.use("/users", usersRoutes);
 
